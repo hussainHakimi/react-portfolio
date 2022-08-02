02ce6282e3830695eec5b597102612310961f28b
hussainHakimi <hussainbayan614@gmail.com>
2022-08-02 18:47:20 +0430

    basic configuration is done(basic files added, general style added.)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+
 
 const App = () => {
   return (
-    <div>Hello World from App.jsx</div>
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonials/>
+    <Contact/>
+    <Footer/>
+
+    </>
   )
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default header
