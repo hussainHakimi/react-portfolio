afe33f0a4d1a955dfd45ad52a8e9e1d930cf379a
hussainHakimi <hussainbayan614@gmail.com>
2022-08-04 18:00:01 +0430

    social media is done.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
 
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am </h5>
+        <h1>Hasiba Ebrahimi</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        
+          <HeaderSocial/>
+
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
