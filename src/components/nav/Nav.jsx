5b562125d95f1bb8068cb4eba5ce542a20531d74
hussainHakimi <hussainbayan614@gmail.com>
2022-08-05 00:41:53 +0430

    Navbar is done.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiCustomerService2Line} from 'react-icons/ri'
+import {BiMessageRounded} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className={activeNav === '#' ? 'active' : ''} onClick={() => {setActiveNav('#')}}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => {setActiveNav('#about')}} className={activeNav ==='#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => {setActiveNav('#experience')}} className={activeNav ==='#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick={() => {setActiveNav('#services')}} className={activeNav ==='#services' ? 'active' : ''}><RiCustomerService2Line/></a>
+      <a href="#contact" onClick={() => {setActiveNav('#contact')}} className={activeNav ==='#contact' ? 'active' : ''}><BiMessageRounded/></a>
+    </nav>
   )
 }
 
