28fc1170747d0334f3d412d3fb9d6fa1cd8e0740
hussainHakimi <hussainbayan614@gmail.com>
2022-08-11 00:00:27 +0430

    start working on experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs'
 import './experience.css'
 
 const Experience = () => {
   return (
     <section id="experience">
-      Experience
+      <h5>What Experience I Have?</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Front Development</h3>
+          <div className="experience__content">
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>HTML5</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>CSS3</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Javascript</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Tailwindcss</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>React</h4>
+              <small>Intermidat</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>PHP</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Laravel</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Python</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Django</h4>
+              <small>Master</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>MYSQL</h4>
+              <small>Intermidat</small>
+            </article>
+          </div>
+        </div>
+
+      </div>
 
     </section>
   )
