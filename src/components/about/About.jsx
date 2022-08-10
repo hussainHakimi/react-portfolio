cb88545a6bc250d060b9b31f0e034bef15d67a09
hussainHakimi <hussainbayan614@gmail.com>
2022-08-10 15:16:42 +0430

    about section is start.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './about.css'
+import ABI from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
+      <div className="container about__container"></div>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ABI} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>+2 Years Working</small>
+            </article>
+          </div>
+        </div>
     </section>
   )
 }
