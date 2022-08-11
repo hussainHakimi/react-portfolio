ae4038d6a26632212d528d58e02e2d3a1fcb1041
hussainHakimi <hussainbayan614@gmail.com>
2022-08-11 09:14:04 +0430

    service section is done for large screen.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,103 @@
 import React from 'react'
 import './services.css'
+import {BiCheckDouble} from 'react-icons/bi'
 
 const Services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5>
+      <h2>Our Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UX/UI Design</h3>
+          </div>
+          <ul>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheckDouble className='service__icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+
+        </article>
+      </div>
     </section>
   )
 }
