84daa0ad0e9bd42f1d753242368a0bdbc0153fc6
hussainHakimi <hussainbayan614@gmail.com>
2022-08-17 15:42:50 +0430

    some changes added to header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/202.png'
 import HeaderSocial from './HeaderSocial'
 
 const header = () => {
@@ -9,7 +9,7 @@ const header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I am </h5>
-        <h1>Hasiba Ebrahimi</h1>
+        <h1>Hussain Hakimi</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         
